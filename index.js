474d420d0a6345f4c5e70ecb20b08ba3413c8f44
Gabriel Lucas <gabriellucas2301@gmail.com>
2024-04-26 15:35:21 -0300

    Feat: FInal Commit

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-const visualizar = document.querySelector(".eye-open");
-const esconder = document.querySelector(".eye-close");
+// Selecionando os elementos necessários do DOM
+const visualizar = document.querySelector(".eye-open"); // Botão de visualização da senha
+const esconder = document.querySelector(".eye-close"); // Botão de ocultação da senha
+const password = document.getElementById("input-password"); // Campo de senha
+const iconToggle = document.querySelector(".icon-toggle"); // Ícone do botão de alternância
+const btnToggle = document.querySelector(".toggle"); // Botão de alternância de tema
 
-const password = document.getElementById("input-password");
-
-const iconToggle = document.querySelector(".icon-toggle");
-
-const btnToggle = document.querySelector(".toggle");
-
+// Adicionando um evento de clique para exibir a senha
 esconder.addEventListener("click", function () {
-  password.type = "text";
-  esconder.classList.add("hidden");
-  visualizar.classList.remove("hidden");
+  password.type = "text"; // Altera o tipo do campo de senha para texto visível
+  esconder.classList.add("hidden"); // Adiciona a classe 'hidden' para ocultar o botão de ocultação
+  visualizar.classList.remove("hidden"); // Remove a classe 'hidden' para exibir o botão de visualização
 });
 
+// Adicionando um evento de clique para ocultar a senha
 visualizar.addEventListener("click", function () {
-  password.type = "password";
-  esconder.classList.remove("hidden");
-  visualizar.classList.add("hidden");
+  password.type = "password"; // Altera o tipo do campo de senha para senha oculta
+  esconder.classList.remove("hidden"); // Remove a classe 'hidden' para exibir o botão de ocultação
+  visualizar.classList.add("hidden"); // Adiciona a classe 'hidden' para ocultar o botão de visualização
 });
 
+// Adicionando um evento de clique para alternar entre temas claro e escuro
 btnToggle.addEventListener("click", function () {
-  const toggleIconMoon = document.querySelector(".img-icon");
+  const toggleIconMoon = document.querySelector(".img-icon"); // Seleciona o ícone de alternância
 
+  // Verifica qual ícone está sendo exibido e altera para o outro
   if (
     toggleIconMoon.src.includes(
       "https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-sun-100-most-used-icons-flaticons-flat-flat-icons-2"
     )
   ) {
-    toggleIconMoon.src = "https://img.icons8.com/color/48/full-moon.png";
-    toggleIconMoon.alt = "Lua";
+    toggleIconMoon.src = "https://img.icons8.com/color/48/full-moon.png"; // Altera o ícone para a lua
+    toggleIconMoon.alt = "Lua"; // Altera o texto alternativo para "Lua"
   } else {
     toggleIconMoon.src =
-      "https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-sun-100-most-used-icons-flaticons-flat-flat-icons-2";
-    toggleIconMoon.alt = "sol";
+      "https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-sun-100-most-used-icons-flaticons-flat-flat-icons-2"; // Altera o ícone para o sol
+    toggleIconMoon.alt = "sol"; // Altera o texto alternativo para "sol"
   }
-  iconToggle.classList.toggle("enable");
-  document.querySelector(".input").classList.toggle("input-dark");
-  document.getElementById("input-password").classList.toggle("input-dark");
 
-  const btnSocial = document.querySelectorAll(".btn-social");
+  // Alternância de classes para alterar o tema entre claro e escuro
+  iconToggle.classList.toggle("enable"); // Altera a classe do ícone de alternância
+  document.querySelector(".input").classList.toggle("input-dark"); // Altera a classe do campo de entrada
+  document.getElementById("input-password").classList.toggle("input-dark"); // Altera a classe do campo de senha
 
+  const btnSocial = document.querySelectorAll(".btn-social"); // Seleciona todos os botões sociais
+
+  // Alternância de classes para os botões sociais entre claro e escuro
   btnSocial.forEach((btnSocial) => {
-    btnSocial.classList.toggle("btn-dark-social");
+    btnSocial.classList.toggle("btn-dark-social"); // Altera a classe de cada botão social
   });
 
-  document.querySelector(".container").classList.toggle("container-dark");
-  document.querySelector(".email").classList.toggle("label-dark");
-  document.querySelector(".password").classList.toggle("label-dark");
-  document.body.classList.toggle("body-dark");
+  // Alternância de classes para os elementos do formulário entre claro e escuro
+  document.querySelector(".container").classList.toggle("container-dark"); // Altera a classe do contêiner
+  document.querySelector(".email").classList.toggle("label-dark"); // Altera a classe do rótulo de e-mail
+  document.querySelector(".password").classList.toggle("label-dark"); // Altera a classe do rótulo de senha
+  document.body.classList.toggle("body-dark"); // Altera a classe do corpo da página
 });
---
+++ readme.md
@@ -0,0 +1,53 @@
+<h1 align="center">Página de Login Personalizada</h1>
+
+<div align="center">
+    <div class="imagens" style="display: grid; grid-template-areas: 'img1 img2';">
+        <img height="100%"  src="./src/light-mode.png" alt="Modo Claro" style="grid-area: img1;" />
+        <img height="100%" src="./src/dark-mode.png" alt="Modo Escuro" style="grid-area: img2;" />
+        <!-- <img width="60%" src="./src/mobile.png" alt="Versão Mobile" style="grid-area: img3;" /> -->
+    </div>
+</div>
+
+---
+
+<div align="center">
+
+![GitHub repo size](https://img.shields.io/github/repo-size/DevGabriel0402/Tela-de-Login?style=for-the-badge)
+![GitHub issues](https://img.shields.io/github/issues/DevGabriel0402/Tela-de-Login?style=for-the-badge)
+![GitHub](https://img.shields.io/github/license/DevGabriel0402/Tela-de-Login?style=for-the-badge)
+
+</div>
+
+---
+
+<p align="center">Este é um projeto de página de login personalizada com funcionalidades avançadas, como exibição/ocultação de senha e alternância de temas claro/escuro.</p>
+
+## Funcionalidades
+
+- **Exibição/Ocultação da Senha**: Os usuários podem alternar entre a exibição e a ocultação da senha ao clicar no ícone apropriado.
+- **Alternância de Tema Claro/Escuro**: Os usuários podem alternar entre os temas claro e escuro da página de login com um simples clique no botão de alternância.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Utilizar
+
+1. Clone o repositório: `git clone https://github.com/DevGabriel0402/Tela-de-Login.git`
+2. Abra o arquivo `index.html` em seu navegador.
+
+## Contribuição
+
+Contribuições são bem-vindas! Para sugestões de melhorias, abra uma issue. Se deseja contribuir diretamente, siga estas etapas:
+
+1. Fork o projeto
+2. Crie uma nova branch (`git checkout -b feature/sua-feature`)
+3. Faça commit de suas alterações (`git commit -am 'Adicione sua feature'`)
+4. Faça push para a branch (`git push origin feature/sua-feature`)
+5. Abra um Pull Request
+
+## Licença
+
+Este projeto está licenciado sob a Licença MIT - veja o arquivo [LICENSE](LICENSE) para detalhes.
